9f802c70bf6980e8c0be20da0ba53f6c31829bba
Eric M <ericmarchese77@gmail.com>
2015-03-01 21:07:44 -0500

    Visible cards, bug fixes

--- blackjack.rb
+++ blackjack.rb
@@ -1,6 +1,8 @@
 # Double-Deck Blackjack in Ruby
 # https://github.com/em77
 
+require_relative 'deck'
+
 class Float
 	def dec_drop
 		to_i == self ? to_i : self
@@ -10,26 +12,13 @@ end
 continue = "y"
 bet = 0
 bankroll = 100.0
-holecard = 0
-upcard = 0
-playercard1 = 0
-playercard2 = 0
+holecard = {}
+upcard = {}
+playercard1 = {}
+playercard2 = {}
 player_ace_count = 0
 dealer_ace_count = 0
-deck = [2,2,2,2,
-				3,3,3,3,
-				4,4,4,4,
-				5,5,5,5,
-				6,6,6,6,
-				7,7,7,7,
-				8,8,8,8,
-				9,9,9,9,
-				10,10,10,10,  # 10s
-				10,10,10,10,  # Jacks
-				10,10,10,10,  # Queens
-				10,10,10,10,  # Kings
-				11,11,11,11]
-cards = (deck * 2).shuffle
+cards = (@deck * 2).shuffle
 player_hand = []
 dealer_hand = []
 
@@ -38,7 +27,7 @@ puts "Minimum Bet: $5\n\n"
 sleep 1
 puts "We are just beginning a new deck."
 sleep 1
-cards.delete_at(cards.count-1)
+cards.delete_at(0)
 puts "One card burned...\n"
 sleep 1
 
@@ -48,11 +37,11 @@ while continue == "y"
 	player_ace_count = 0
 	dealer_ace_count = 0
 	if cards.count <= 11
-		cards = (deck * 2)
-		cards = cards.shuffle
+		cards = (@deck * 2)
+		cards.shuffle!
 		sleep 2
 		puts "\nCards are being shuffled...\n"
-		cards.delete_at(cards.count-1)
+		cards.delete_at(0)
 		puts "One card burned...\n"
 		sleep 1
 	end
@@ -65,85 +54,100 @@ while continue == "y"
 		puts "\nMinimum bet is $5 and smallest chip size is 50¢"
 		print "\nEnter your bet amount: "
 		bet = gets.to_f
-		print "\n"
 	end
 
 	dealer_hand = []
 	player_hand = []
-	dealer_hand << (holecard = cards.delete_at(cards.count - 1))
-	dealer_hand << (upcard = cards.delete_at(cards.count - 1))
-	player_hand << (playercard1 = cards.delete_at(cards.count - 1))
-	player_hand << (playercard2 = cards.delete_at(cards.count - 1))
-	playercard = 0
-	dealercard = 0
-	dealer_total = dealer_hand.inject(:+)
-	player_total = player_hand.inject(:+)
+	dealer_hand << (holecard = cards.delete_at(0))
+	dealer_hand << (upcard = cards.delete_at(0))
+	player_hand << (playercard1 = cards.delete_at(0))
+	player_hand << (playercard2 = cards.delete_at(0))
+	playercard = {}
+	dealercard = {}
+	dealer_total = dealer_hand.reduce(0) {|total,c| total + c[:value]}
+	player_total = player_hand.reduce(0) {|total,c| total + c[:value]}
 
-	if (playercard1 && playercard2) == 11
+	if (playercard1[:value] && playercard2[:value]) == 11
 		player_ace_count = player_ace_count + 1
-		playercard1 = 1
-	elsif (playercard1 || playercard2) == 11
+		playercard1[:value] = 1
+	elsif (playercard1[:value] || playercard2[:value]) == 11
 		player_ace_count = player_ace_count + 1
 	end
 
 	if dealer_total == 21
 		sleep 1
-		puts "\nDealer has blackjack! The holecard was #{holecard}."
+		card_names_p = (player_hand.map{|c| c[:name]})
+		card_names_d = (dealer_hand.map{|c| c[:name]})
+		puts "\nPlayer hand: #{card_names_p.join(', ')}"
+		puts "Player total: #{player_total}\n"
+		puts "\nDealer hand: #{card_names_d.join(', ')}"
+		puts "Dealer total: #{dealer_total}\n"
+		puts "\nDealer has blackjack! The holecard was #{holecard[:name]}."
 		puts "You lost $#{bet.dec_drop}"
 		bankroll = bankroll - bet
 	elsif player_total == 21
 		sleep 1
-		puts "\nPlayer has blackjack! The holecard was #{holecard}."
-		puts "You won $#{(bet*2).dec_drop}!"
+		puts "\nPlayer has blackjack! The holecard was #{holecard[:name]}."
+		puts "You won $#{(bet*2.5).dec_drop}!"
 		bankroll = bankroll + (bet*2)
 	else
 		while player_total <= 20
 			if decision == "h"
 				sleep 1
-				puts "\nDealer upcard: #{upcard}"
+				puts "Dealer upcard: #{upcard[:name]}\n"
 				sleep 1
-				if player_hand.include?(11)
-					puts "Player hand: Soft #{player_total}"
+				card_names_p = (player_hand.map{|c| c[:name]})
+				location_p = player_hand.index {|c| c[:value] == 11}
+				if (location_p != nil)
+					puts "\nPlayer hand: #{card_names_p.join(', ')}"
+					puts "Player total: Soft #{player_total}"
 					sleep 1
 				else
-					puts "Player hand: #{player_total}"
+					puts "\nPlayer hand: #{card_names_p.join(', ')}"
+					puts "Player total: #{player_total}"
 					sleep 1
 				end
+
 				if round_counter == 0
 					print "\nType \"h\" to hit, \"d\" to double down, \"s\" to stand and \"sur\" to surrender: "
+					decision = gets.chomp
+					while (["h", "d", "s", "sur"].include? decision) == false
+						print "\nThat is not a valid choice. Enter \"h,\" \"d,\" \"s\" or \"sur\": "
+						decision = gets.chomp
+					end
 				else
 					print "\nType \"h\" to hit and \"s\" to stand: "
-				end
-				decision = gets.chomp
-				while (["h", "d", "s", "sur"].include? decision) == false
-					print "\nThat is not a valid choice. Enter \"h,\" \"d,\" \"s\" or \"sur\": "
 					decision = gets.chomp
+					while (["h", "s"].include? decision) == false
+						print "\nThat is not a valid choice. Enter \"h\" or \"s\": "
+						decision = gets.chomp
+					end
 				end
+
 				while (decision == "d") && (bankroll < (bet*2))
 					puts "\nYou don't have enough money to double down."
 					print "\nPlease enter \"h,\" \"s\" or \"sur\": "
 					decision = gets.chomp
 				end
 				if (decision == "h") || ((decision == "d") && (round_counter == 0))
-					playercard = cards.delete_at(cards.count - 1)
-					if (playercard == 11)
+					playercard = cards.delete_at(0)
+					if (playercard[:value] == 11)
 						player_ace_count = player_ace_count + 1
-						if (player_ace_count == 1) && ((playercard + player_total) > 21)
-							playercard = 1
+						if (player_ace_count == 1) && ((playercard[:value] + player_total) > 21)
+							playercard[:value] = 1
 							player_ace_count = player_ace_count - 1
 						elsif player_ace_count >= 2
-							playercard = 1
+							playercard[:value] = 1
 							player_ace_count = player_ace_count - 1
 						end
 					end
-					if ((player_total + playercard) > 21) && (player_hand.include?(11))
-						player_hand[player_hand.index(11)] = 1
-						if player_hand.include?(11)
-							puts "\n\nerror with player\n\n"
-						end
+					location_p = player_hand.index(player_hand.index {|c| c[:value] == 11})
+					if ((player_total + playercard[:value]) > 21) && 
+					(location_p != nil)
+						player_hand[location_p][:value] = 1
 					end
 					player_hand << playercard
-					player_total = player_hand.inject(:+)
+					player_total = player_hand.reduce(0) {|total,c| total + c[:value]}
 					if decision == "d"
 						bet = bet*2
 					end
@@ -153,66 +157,100 @@ while continue == "y"
 				break
 			end
 		end
-		if decision == "sur"
+		while dealer_total <= 16
+			dealercard = cards.delete_at(0)
+			if (dealercard[:value] == 11)
+				dealer_ace_count = dealer_ace_count + 1
+				if (dealer_ace_count == 1) && ((dealercard[:value] + dealer_total) > 21)
+					dealercard[:value] = 1
+					dealer_ace_count = dealer_ace_count - 1
+				elsif dealer_ace_count >= 2
+					dealercard[:value] = 1
+					dealer_ace_count = dealer_ace_count - 1
+				end
+			end
+
+			location_d = dealer_hand.index {|c| c[:value] == 11}
+			dealer_total = dealer_hand.reduce(0) {|total,c| total + c[:value]}
+			if ((dealer_total + dealercard[:value]) > 21) && (location_d != nil)
+				dealer_hand[location_d][:value] = 1
+			end
+
+			dealer_hand << dealercard
+			dealer_total = dealer_hand.reduce(0) {|total,c| total + c[:value]}
+		end
+		if (decision == "sur")
+			card_names_p = (player_hand.map{|c| c[:name]})
+			card_names_d = (dealer_hand.map{|c| c[:name]})
+			puts "\nPlayer hand: #{card_names_p.join(', ')}"
+			puts "Player total: #{player_total}\n"
+			puts "\nDealer hand: #{card_names_d.join(', ')}"
+			puts "Dealer total: #{dealer_total}\n"
+			sleep 1
 			puts "\nYou surrendered and #{bet/2} was returned to your bankroll."
-			puts "The holecard was #{holecard}"
+			puts "The holecard was #{holecard[:name]}"
 			bankroll = bankroll - (bet/2)
 		elsif player_total > 21
+			sleep 1
+			card_names_p = (player_hand.map{|c| c[:name]})
+			card_names_d = (dealer_hand.map{|c| c[:name]})
+			puts "\nPlayer hand: #{card_names_p.join(', ')}"
+			puts "Player total: #{player_total}\n"
+			puts "\nDealer hand: #{card_names_d.join(', ')}"
+			puts "Dealer total: #{dealer_total}\n"
 			sleep 1
 			puts "\nPlayer busted with #{player_total}"
 			puts "You lost $#{bet.dec_drop}"
 			bankroll = bankroll - bet
+		elsif dealer_total > 21
+			sleep 1
+			card_names_p = (player_hand.map{|c| c[:name]})
+			card_names_d = (dealer_hand.map{|c| c[:name]})
+			puts "\nPlayer hand: #{card_names_p.join(', ')}"
+			puts "Player total: #{player_total}\n"
+			sleep 1
+			puts "\nDealer hand: #{card_names_d.join(', ')}"
+			puts "Dealer total: #{dealer_total}\n"
+			sleep 1
+			puts "\nDealer busted with #{dealer_total} and the holecard was #{holecard[:name]}."
+			puts "You won $#{(bet*2).dec_drop}!"
+			bankroll = bankroll + (bet*2)
+		elsif dealer_total > player_total
+			sleep 1
+			card_names_p = (player_hand.map{|c| c[:name]})
+			card_names_d = (dealer_hand.map{|c| c[:name]})
+			puts "\nPlayer hand: #{card_names_p.join(', ')}"
+			puts "Player total: #{player_total}\n"
+			sleep 1
+			puts "\nDealer hand: #{card_names_d.join(', ')}"
+			puts "Dealer total: #{dealer_total}\n"
+			sleep 1
+			puts "\nDealer won with #{dealer_total} and the holecard was #{holecard[:name]}."
+			puts "You lost $#{bet.dec_drop}"
+			bankroll = bankroll - bet
+		elsif dealer_total == player_total
+			sleep 1
+			card_names_p = (player_hand.map{|c| c[:name]})
+			card_names_d = (dealer_hand.map{|c| c[:name]})
+			puts "\nPlayer hand: #{card_names_p.join(', ')}"
+			puts "Player total: #{player_total}\n"
+			sleep 1
+			puts "\nDealer hand: #{card_names_d.join(', ')}"
+			puts "Dealer total: #{dealer_total}\n"
+			sleep 1
+			puts "\nIt's a push! The holecard was #{holecard[:name]}."
 		else
-			while dealer_total <= 16
-				dealercard = cards.delete_at(cards.count - 1)
-				if (dealercard == 11)
-					dealer_ace_count = dealer_ace_count + 1
-					if (dealer_ace_count == 1) && ((dealercard + dealer_total) > 21)
-						dealercard = 1
-						dealer_ace_count = dealer_ace_count - 1
-					elsif dealer_ace_count >= 2
-						dealercard = 1
-						dealer_ace_count = dealer_ace_count - 1
-					end
-				end
-				dealer_total = dealer_hand.inject(:+)
-				if ((dealer_total + dealercard) > 21) && (dealer_hand.include?(11))
-					dealer_hand[dealer_hand.index(11)] = 1
-					if dealer_hand.include?(11)
-						puts "\n\nerror with dealer\n\n"
-					end
-				end
-
-				dealer_hand << dealercard
-				dealer_total = dealer_hand.inject(:+)
-			end
-			if dealer_total > 21
-				sleep 1
-				puts "\nDealer busted with #{dealer_total} and the holecard was #{holecard}."
-				puts "You won $#{(bet*2).dec_drop}!"
-				bankroll = bankroll + (bet*2)
-			elsif dealer_total > player_total
-				sleep 1
-				puts "\nPlayer hand: #{player_total}"
-				puts "Dealer won with #{dealer_total} and the holecard was #{holecard}."
-				puts "You lost $#{bet.dec_drop}"
-				bankroll = bankroll - bet
-			elsif dealer_total == player_total
-				sleep 1
-				puts "\nPlayer hand: #{player_total}"
-				sleep 1
-				puts "Dealer hand: #{dealer_total}"
-				sleep 1
-				puts "It's a push! The holecard was #{holecard}."
-			else
-				sleep 1
-				puts "\nPlayer hand: #{player_total}"
-				sleep 1
-				puts "Dealer hand: #{dealer_total}"
-				sleep 1
-				puts "You won $#{(bet*2).dec_drop}! The holecard was #{holecard}."
-				bankroll = bankroll + (bet*2)
-			end
+			sleep 1
+			card_names_p = (player_hand.map{|c| c[:name]})
+			card_names_d = (dealer_hand.map{|c| c[:name]})
+			puts "\nPlayer hand: #{card_names_p.join(', ')}"
+			puts "Player total: #{player_total}\n"
+			sleep 1
+			puts "\nDealer hand: #{card_names_d.join(', ')}"
+			puts "Dealer total: #{dealer_total}\n"
+			sleep 1
+			puts "\nYou won $#{(bet*2).dec_drop}! The holecard was #{holecard[:name]}."
+			bankroll = bankroll + (bet*2)
 		end
 	end
 	sleep 1
---
+++ deck.rb
@@ -0,0 +1,15 @@
+@deck = [
+				{:name => "2-h", :value => 2}, {:name => "2-d", :value => 2}, {:name => "2-s", :value => 2}, {:name => "2-c", :value => 2},
+				{:name => "3-h", :value => 3}, {:name => "3-d", :value => 3}, {:name => "3-s", :value => 3}, {:name => "3-c", :value => 3},
+				{:name => "4-h", :value => 4}, {:name => "4-d", :value => 4}, {:name => "4-s", :value => 4}, {:name => "4-c", :value => 4},
+				{:name => "5-h", :value => 5}, {:name => "5-d", :value => 5}, {:name => "5-s", :value => 5}, {:name => "5-c", :value => 5},
+				{:name => "6-h", :value => 6}, {:name => "6-d", :value => 6}, {:name => "6-s", :value => 6}, {:name => "6-c", :value => 6},
+				{:name => "7-h", :value => 7}, {:name => "7-d", :value => 7}, {:name => "7-s", :value => 7}, {:name => "7-c", :value => 7},
+				{:name => "8-h", :value => 8}, {:name => "8-d", :value => 8}, {:name => "8-s", :value => 8}, {:name => "8-c", :value => 8},
+				{:name => "9-h", :value => 9}, {:name => "9-d", :value => 9}, {:name => "9-s", :value => 9}, {:name => "9-c", :value => 9},
+				{:name => "10-h", :value => 10}, {:name => "10-d", :value => 10}, {:name => "10-s", :value => 10}, {:name => "10-c", :value => 10},
+				{:name => "K-h", :value => 10}, {:name => "K-d", :value => 10}, {:name => "K-s", :value => 10}, {:name => "K-c", :value => 10},
+				{:name => "J-h", :value => 10}, {:name => "J-d", :value => 10}, {:name => "J-s", :value => 10}, {:name => "J-c", :value => 10},
+				{:name => "Q-h", :value => 10}, {:name => "Q-d", :value => 10}, {:name => "Q-s", :value => 10}, {:name => "Q-c", :value => 10},
+				{:name => "A-h", :value => 11}, {:name => "A-d", :value => 11}, {:name => "A-s", :value => 11}, {:name => "A-c", :value => 11}
+						]
